45e722532906828a26e9f840a78aa50b27ef78aa
Sandrah <comfortsandrah13@gmail.com>
2024-04-30 22:54:25 +0300

    Anonymous Functions in dart.

---
+++ AnonymousFunctions.dart
@@ -0,0 +1,12 @@
+void main() {
+  List<String> cars = ["BMW", "AUDI", "MERCIDEZ", "ROLLS ROYCE"];
+  cars.forEach((car) {
+    print(car);
+  });
+
+  
+  const fruits = ["Apples", "Bananas", "Oranges", "Kiwi"];
+  fruits.forEach((fruit) {
+    print(fruit);
+  });
+}
